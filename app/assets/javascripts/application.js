9072c390e0ff19c0b5a8a2788d4d3a352ecd317f
Matt <matt.fasano88@gmail>
2017-05-28 12:51:44 -0400

    thereisagod

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,68 +14,10 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
 document.addEventListener("DOMContentLoaded", function(event) {
 console.log("DOM fully loaded and parsed");
-    console.log(acceptCall)
-
-  kandy.setup();
-
-  var registration = document.querySelector('#registration')
-  var login = document.querySelector('#login')
-  var output1 = document.querySelector('#output1')
-  var output2 = document.querySelector('#output2')
-
-
-//====================== REGISTER PEOPLE =================
-  registration.addEventListener('submit', function(e) {
-    e.preventDefault()
-    var user = {
-      "user_id": e.target.user_id.value,
-      "user_first_name": e.target.user_first_name.value, // Optional
-      "user_last_name": e.target.user_last_name.value, // Optional
-      "user_password": e.target.user_password.value // Optional, will be generated if ommitted.
-  }
-    // console.log(user)
-    fetch('https://api.kandy.io/v1.2/domains/users/user_id?key=DAT1fd84286cf3649978264b5131b137ced', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(user)
-    })
-    .then(r => r.json())
-    .then(response=>{
-      console.log('Kandy says: ',response)
-      output1.innerHTML = response.message
-    })
-    .catch(error=>{
-      console.log('u fuked up')
-    })
-  })
-
-//====================== LOGIN PEOPLE ====================
-
-//GET https://api.kandy.io/v1.2/domains/users/accesstokens?key=DATfa8a1a2cf85448228810d29a77632335
-
-  login.addEventListener('submit', function (e) {
-    e.preventDefault()
-    kandy.login(
-      'DAK05485860814b4b349096b7013f7ba900',
-      e.target.user_name.value,
-      e.target.user_password.value,
-      function(){
-        output2.innerHTML = "Login succesful"
-      },
-      ()=>output2.innerHTML = "Login failed",
-    )
-  })
-
-
-
-// // ============= LOGIN END =================
-
-  // Setup Kandy to make and receive calls.
+    // Setup Kandy to make and receive calls.
   kandy.setup({
       //  HTML elements for streaming containers.
      remoteVideoContainer: document.getElementById("remote-container"),
@@ -88,6 +30,66 @@ console.log("DOM fully loaded and parsed");
           callEstablished: onCallEstablished,
       }
   });
+
+  var registration = document.querySelector('#registration')
+  var login = document.querySelector('#login')
+  var output1 = document.querySelector('#output1')
+  var output2 = document.querySelector('#output2')
+
+
+//====================== REGISTER PEOPLE =================
+  if (registration) {
+    registration.addEventListener('submit', function(e) {
+      e.preventDefault()
+      var user = {
+        "user_id": e.target.user_id.value,
+        "user_first_name": e.target.user_first_name.value, // Optional
+        "user_last_name": e.target.user_last_name.value, // Optional
+        "user_password": e.target.user_password.value // Optional, will be generated if ommitted.
+      }
+      // console.log(user)
+      fetch('https://api.kandy.io/v1.2/domains/users/user_id?key=DAT1fd84286cf3649978264b5131b137ced', {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: "POST",
+          body: JSON.stringify(user)
+        })
+        .then(r => r.json())
+        .then(response=>{
+          console.log('Kandy says: ',response)
+          output1.innerHTML = response.message
+          window.location = "/home/show"
+        })
+        .catch(error=>{
+          console.log('u fuked up')
+        })
+      })
+
+  }
+
+
+
+//====================== LOGIN PEOPLE ====================
+
+//GET https://api.kandy.io/v1.2/domains/users/accesstokens?key=DATfa8a1a2cf85448228810d29a77632335
+  if (login) {
+    login.addEventListener('submit', function (e) {
+      e.preventDefault()
+      kandy.login(
+        'DAK05485860814b4b349096b7013f7ba900',
+        e.target.user_name.value,
+        e.target.user_password.value,
+        function(){
+          output2.innerHTML = "Login succesful"
+        },
+        ()=>output2.innerHTML = "Login failed",
+      )
+    })
+  }
+
+// // ============= LOGIN END =================
 
     // Utility function for appending messages to the message div.
   function log(message) {
@@ -123,8 +125,6 @@ console.log("DOM fully loaded and parsed");
       document.getElementById("end-call").disabled = false;
   };
 
-  var acceptCall = document.getElementById("acceptCall")
-    acceptCall.addEventListener("click", acceptCall);
 
   // What to do for an incoming call.
   function onCallIncoming(call) {
@@ -138,6 +138,9 @@ console.log("DOM fully loaded and parsed");
       document.getElementById("decline-call").disabled = false;
   };
 
+  var acceptCallElement = document.getElementById("acceptCall")
+    acceptCallElement.addEventListener("click", acceptCall);
+
     //  Accept an incoming call.
     function acceptCall() {
       // Tell Kandy to answer the call.
@@ -150,8 +153,8 @@ console.log("DOM fully loaded and parsed");
       document.getElementById("decline-call").disabled = true;
     };
 
-  var videoShow = document.getElementById("showVideo")
-    videoShow.addEventListener("click", toggleVideo)
+  var videoShowElement = document.getElementById("showVideo")
+    videoShowElement.addEventListener("click", toggleVideo)
     // Show or hide video, depending on current status.
   function toggleVideo() {
       if(showVideo) {
